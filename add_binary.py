fcbb0393185295228edbc1dfa16f21a9560f57f1
Aaron Bittel <aaron@bittels.de>
2025-02-12 16:11:17 +0100

    add binary in python

---
+++ add_binary.py
@@ -0,0 +1,49 @@
+from collections import namedtuple
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = ""
+
+        if len(a) < len(b):
+            a, b = b, a
+        b = "0" * (len(a) - len(b)) + b
+
+        carryover = 0
+
+        for n1, n2 in zip(a[::-1], b[::-1]):
+            match (n1, n2, carryover):
+                case ("1", "1", 0):
+                    res += "0"
+                    carryover = 1
+                case ("1", "1", 1):
+                    res += "1"
+                    carryover = 1
+                case ("1", "0", 0) | ("0", "1", 0):
+                    res += "1"
+                    carryover = 0
+                case ("1", "0", 1) | ("0", "1", 1):
+                    res += "0"
+                    carryover = 1
+                case ("0", "0", 0):
+                    res += "0"
+                    carryover = 0
+                case ("0", "0", 1):
+                    res += "1"
+                    carryover = 0
+
+        if carryover == 1:
+            res += "1"
+
+        return res[::-1]
+
+
+tests = namedtuple("Test", ["ins", "out"])
+tests = [tests(ins=("11", "1"), out="100"), tests(ins=("1010", "1011"), out="10101")]
+
+for tt in tests:
+    got = Solution().addBinary(*tt.ins)
+    if got != tt.out:
+        print(f"Ins: {tt.ins} -> Expected: {tt.out}, got: {got}")
+    else:
+        print(f"Ins: {tt.ins} -> {got}: Success!")
