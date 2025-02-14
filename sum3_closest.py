a4639513ded6c97b17b684b0ca4f170e563b73e7
Aaron Bittel <aaron@bittels.de>
2025-02-14 11:34:50 +0100

    sum3 closest in python

---
+++ sum3_closest.py
@@ -0,0 +1,44 @@
+# Solution
+# 3sum!!!
+# 1. sort numbers
+# 2. go through each number
+#   - l (first after cur), r (last) pointer
+#   - cal cur_sum, cal diff
+#   - if diff < 0 -> l++ (make sum bigger) else r-- (make sum smaller)
+# Runtime: O(n**2) -> n * (n - i)
+
+from collections import namedtuple
+
+
+class Solution:
+    tests = namedtuple("Test", ["ins", "out"])
+
+    TESTS = [
+        tests(ins=([-1, 2, 1, -4], 1), out=2),
+        tests(ins=([0, 0, 0], 1), out=0),
+        tests(ins=([0, 1, 2], 0), out=3),
+    ]
+
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
+        nums.sort()
+
+        min_sum = sum(nums[:3])
+        min_diff = abs(target - min_sum)
+
+        for i, n in enumerate(nums):
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                cur_sum = n + nums[l] + nums[r]
+                diff = cur_sum - target
+
+                if diff < 0:
+                    l += 1
+                    diff = -diff
+                else:
+                    r -= 1
+
+                if diff < min_diff:
+                    min_diff = diff
+                    min_sum = cur_sum
+
+        return min_sum
